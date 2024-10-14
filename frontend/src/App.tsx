cf945b2c6011021f3881fe88736e8658a607939d
kristijanjakimov <kiko.jakimov@gmail.com>
2024-10-14 11:14:23 +0200

    Endpoint called

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useEffect, useState} from 'react'
 import './App.css'
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [hOneState, setHOneState] = useState<string | undefined>("Hi");
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const callTheEndpoint = async () => {
+        try {
+            const response = await axios.get<string>(`/api/hello`);
+            setHOneState(response.data);
+            return response.data;
+        } catch (err) {
+            console.log("Error occurred: " + err);
+        }
+    };
+
+    return (
+        <>
+            <h1>{hOneState}</h1>
+            <div className="card">
+                <button onClick={async () => {
+                    if (hOneState === "Hi") {
+                        setHOneState(await callTheEndpoint())
+                    }
+                    else {
+                        setHOneState("Hi")
+                    }
+                }}>
+                    Call the API
+                </button>
+            </div>
+        </>
+    )
 }
 
 export default App
